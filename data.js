1263baa4d1a6a14b5bd7bccc4ea26f45390eb530
101walaowe101 <mr.cheongzhiteng@gmail.com>
2026-09-17 22:11:39 +0800

    Create data.js with song list for Nanatsukaze
    Added a list of songs by Nanatsukaze with YouTube links.

---
+++ data.js
@@ -0,0 +1,39 @@
+// data.js
+// Nanatsukaze 全部歌曲列表
+// 请将每首歌的 YouTube 链接填入 youtubeLink 字段
+// 如果某首歌暂时没有YouTube链接，可以留空，播放器会自动跳过
+
+const songs = [
+  { title: "Connect the World", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=YOUR_VIDEO_ID" },
+  { title: "Harumeku", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=vCDCepKTQ-Q" },
+  { title: "Vacuum City", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "もしも", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "Aria", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "Replay", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "Hazy Moon Night", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "Dream Gravity", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "Sentimental Life", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "End of the Summer", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "Goodbye Wonderland", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "僕らのアイデンティティ", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "七ツ風", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "空飛ぶ猫", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "機械生命体", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "夏が終わる", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "SOS", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "朧月夜", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "さよならワンダーランド", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "言の針", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "夢重力", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "感傷生命", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "空耳", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "五月猫", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "What If (w/Dankidz)", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "青さは止んだ", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "四面楚歌", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "ららら", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "Android", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "I Can't Wait feat. GUMI", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "透明なパレット feat. 初音MIKU", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "Sakura rain", artist: "Nanatsukaze", youtubeLink: "" }
+];
