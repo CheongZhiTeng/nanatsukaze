d4e5249b579c42fe24d83a0d4047d3bbfdb798c3
101walaowe101 <mr.cheongzhiteng@gmail.com>
2026-09-17 22:15:57 +0800

    Revise song list and update YouTube links
    Updated song list with new YouTube links and added new songs.

--- data.js
+++ data.js
@@ -1,39 +1,30 @@
 // data.js
-// Nanatsukaze 全部歌曲列表
-// 请将每首歌的 YouTube 链接填入 youtubeLink 字段
-// 如果某首歌暂时没有YouTube链接，可以留空，播放器会自动跳过
+// Nanatsukaze 全部歌曲列表 (English Titles)
+// youtubeLink 为空的歌曲，播放器会自动跳过
+// 找到新链接时，直接填入对应字段即可
 
 const songs = [
-  { title: "Connect the World", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=YOUR_VIDEO_ID" },
-  { title: "Harumeku", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=vCDCepKTQ-Q" },
+  { title: "Connect the World", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=zyzR7XHeJsQ" },
+  { title: "Harumeku", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/live/vCDCepKTQ-Q" },
+  { title: "Aria", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/live/P7ueDJG9IO4" },
+  { title: "Replay", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/live/wPOEcVWN5Ko" },
+  { title: "Reincarnation", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=-VITOjLP7Us" },
+  { title: "Our Identity", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=ertwyT4gnc0" },
+  { title: "End of the Summer", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=gsqRwAOfOnw" },
+  { title: "Hazy Moon Night", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/svhAjo4X0yc" },
+  { title: "Dream Gravity", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=1TaEXrdCp6A" },
+  { title: "Save Our Sound", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=2YrkJsR83S8" },
   { title: "Vacuum City", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "もしも", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "Aria", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "Replay", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "Hazy Moon Night", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "Dream Gravity", artist: "Nanatsukaze", youtubeLink: "" },
   { title: "Sentimental Life", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "End of the Summer", artist: "Nanatsukaze", youtubeLink: "" },
   { title: "Goodbye Wonderland", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "僕らのアイデンティティ", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "七ツ風", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "空飛ぶ猫", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "機械生命体", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "夏が終わる", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "SOS", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "朧月夜", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "さよならワンダーランド", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "言の針", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "夢重力", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "感傷生命", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "空耳", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "五月猫", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "What If (w/Dankidz)", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "青さは止んだ", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "四面楚歌", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "ららら", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "Flying Cat", artist: "Nanatsukaze", youtubeLink: "" },
   { title: "Android", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "I Can't Wait feat. GUMI", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "透明なパレット feat. 初音MIKU", artist: "Nanatsukaze", youtubeLink: "" },
-  { title: "Sakura rain", artist: "Nanatsukaze", youtubeLink: "" }
+  { title: "Soramimi", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "May Cat", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "Youth is Gone", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "Sakura Rain", artist: "Nanatsukaze", youtubeLink: "" },  
+  { title: "Messiah", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "Needle of Words", artist: "Nanatsukaze", youtubeLink: "" },
+  { title: "四面楚歌", artist: "Nanatsukaze", youtubeLink: "" },
+
 ];
