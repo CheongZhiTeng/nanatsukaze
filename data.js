d24150579e5bc3af35fd30deb4719032ec14a848
101walaowe101 <mr.cheongzhiteng@gmail.com>
2026-09-19 18:24:15 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,6 @@
 // data.js
-// Nanatsukaze full song list (English titles)
-// Songs with empty youtubeLink are automatically skipped
-// Fill in the URL when you find a link
-
 const songs = [
-  { title: "Connect the World", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/M-IayQIR0XA?si=iqLRjijtHHC_TV_G" },
+  { title: "Connect the World", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/M-IayQIR0XA" },
   { title: "Harumeku", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/live/vCDCepKTQ-Q" },
   { title: "Aria", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/live/P7ueDJG9IO4" },
   { title: "Replay", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/live/wPOEcVWN5Ko" },
@@ -13,13 +9,13 @@ const songs = [
   { title: "End of the Summer", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=gsqRwAOfOnw" },
   { title: "Hazy Moon Night", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/svhAjo4X0yc" },
   { title: "Dream Gravity", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=1TaEXrdCp6A" },
-  { title: "Save Our Sound", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/nLWXur60vC8?si=AN2VTAR0Zpuccm2V" },
-  { title: "Goodbye Wonderland", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/I-ea4Opix3E?si=yU5NyqxxgCTQiNkZ" },
+  { title: "Save Our Sound", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/nLWXur60vC8" },
+  { title: "Goodbye Wonderland", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/I-ea4Opix3E" },
   { title: "Flying Cat", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/watch?v=EQgHnfXAMBY" },
-  { title: "Soramimi", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/vJDxS_ovbpc?si=73uLPeCfvWZXYw3n" },
+  { title: "Soramimi", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/vJDxS_ovbpc" },
   { title: "May Cat", artist: "Nanatsukaze", youtubeLink: "https://www.youtube.com/live/xYXIh06fV_w" },
-  { title: "Youth is Gone", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/KOp36bZMp4Q?si=e6YBmSLgfZbA6x9C" },
-  { title: "Sakura Rain", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/4sKjn3ryB14?si=_iAJLjZrofWx_pHS" },
-  { title: "Messiah", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/sX-7C9Xlw0Y?si=7iZekbwrHmWk9YPN" },
-  { title: "Needle of Words", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/7AfVSx3Hov0?si=BAdPhh9vkw0YPsgV" },
+  { title: "Youth is Gone", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/KOp36bZMp4Q" },
+  { title: "Sakura Rain", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/4sKjn3ryB14" },
+  { title: "Messiah", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/sX-7C9Xlw0Y" },
+  { title: "Needle of Words", artist: "Nanatsukaze", youtubeLink: "https://youtu.be/7AfVSx3Hov0" }
 ];
